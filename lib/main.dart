848124bfa8606e278fc1d781c5f2676688a120e5
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-12-28 18:49:34 +0700

    Created widget bottom sheet mini player

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 void main() => runApp(App());
 
@@ -44,6 +46,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
     double widthScreen = mediaQueryData.size.width;
+    double heightScreen = mediaQueryData.size.height;
     double paddingBottom = mediaQueryData.padding.bottom;
 
     return Scaffold(
@@ -54,7 +57,7 @@ class MyApp extends StatelessWidget {
             Column(
               children: <Widget>[
                 _buildWidgetBackgroundCoverAlbum(widthScreen, context),
-                _buildWidgetListMusic(context, paddingBottom),
+                _buildWidgetListMusic(context, paddingBottom, widthScreen, heightScreen),
               ],
             ),
             _buildWidgetButtonPlayAll(),
@@ -64,7 +67,7 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Widget _buildWidgetListMusic(BuildContext context, double paddingBottom) {
+  Widget _buildWidgetListMusic(BuildContext context, double paddingBottom, double widthScreen, double heightScreen) {
     return Expanded(
       child: Container(
         padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: paddingBottom > 0 ? paddingBottom : 16.0),
@@ -73,8 +76,8 @@ class MyApp extends StatelessWidget {
             begin: Alignment.bottomRight,
             end: Alignment.topLeft,
             colors: [
-              Colors.blueGrey.withOpacity(0.5),
-              Colors.white.withOpacity(0.1),
+              Colors.blueGrey.withOpacity(0.7),
+              Colors.white70.withOpacity(0.7),
             ],
             stops: [0.1, 0.9],
           ),
@@ -104,29 +107,34 @@ class MyApp extends StatelessWidget {
                   int durationMinute = music.durationSecond >= 60 ? music.durationSecond ~/ 60 : 0;
                   int durationSecond = music.durationSecond >= 60 ? music.durationSecond % 60 : music.durationSecond;
                   String strDuration = "$durationMinute:$durationSecond";
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                music.title,
-                                style: TextStyle(fontWeight: FontWeight.w500),
-                              ),
-                              Text(
-                                '${music.artist} • $strDuration',
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                            ],
+                  return GestureDetector(
+                    onTap: () {
+                      _showMiniPlayer(context, widthScreen, heightScreen, paddingBottom, music);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  music.title,
+                                  style: TextStyle(fontWeight: FontWeight.w500),
+                                ),
+                                Text(
+                                  '${music.artist} • $strDuration',
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        Icon(Icons.more_vert),
-                      ],
+                          Icon(Icons.more_vert),
+                        ],
+                      ),
                     ),
                   );
                 },
@@ -135,6 +143,18 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  void _showMiniPlayer(BuildContext context, double widthScreen, double heightScreen, double paddingBottom, Music music) {
+    showBottomSheet(
+      context: context,
+      builder: (context) {
+        return WidgetMiniPlayer(music, widthScreen, heightScreen, paddingBottom);
+      },
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
       ),
     );
   }
@@ -291,6 +311,137 @@ class _WidgetRepeatState extends State<WidgetRepeat> {
       child: Icon(
         Icons.repeat,
         color: _isRepeat ? Color(0xFFAE1947) : Color(0xFF000000),
+      ),
+    );
+  }
+}
+
+class WidgetMiniPlayer extends StatefulWidget {
+  final Music music;
+  final double widthScreen;
+  final double heightScreen;
+  final double paddingBottom;
+
+  WidgetMiniPlayer(this.music, this.widthScreen, this.heightScreen, this.paddingBottom);
+
+  @override
+  _WidgetMiniPlayerState createState() => _WidgetMiniPlayerState();
+}
+
+class _WidgetMiniPlayerState extends State<WidgetMiniPlayer> {
+  Timer _timer;
+  double _progressMiniPlayer = 0.0;
+
+  @override
+  void initState() {
+    if (_timer == null || !_timer.isActive) {
+      _timer = Timer.periodic(Duration(seconds: 1), (value) {
+        if (value.tick - 1 == widget.music.durationSecond) {
+          setState(() {
+            _timer.cancel();
+          });
+          return;
+        }
+        setState(() => _progressMiniPlayer = (value.tick / widget.music.durationSecond) * 100);
+      });
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container();
+          },
+          isScrollControlled: true,
+          isDismissible: false,
+        );
+      },
+      child: Container(
+        width: widget.widthScreen,
+        padding: EdgeInsets.only(left: 16.0, top: 4.0, right: 16.0, bottom: widget.paddingBottom > 0 ? widget.paddingBottom : 16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 28.0,
+              height: 4.0,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(99)),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Now Playing',
+                        style: Theme.of(context).textTheme.subtitle.merge(TextStyle(color: Colors.grey)),
+                      ),
+                      Text('${widget.music.title}', style: Theme.of(context).textTheme.title),
+                    ],
+                  ),
+                ),
+                Stack(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 32.0,
+                      height: 32.0,
+                      child: SleekCircularSlider(
+                        appearance: CircularSliderAppearance(
+                          customWidths: CustomSliderWidths(
+                            progressBarWidth: 2.0,
+                            trackWidth: 1.0,
+                            handlerSize: 1.0,
+                            shadowWidth: 1.0,
+                          ),
+                          infoProperties: InfoProperties(
+                            modifier: (value) => '',
+                          ),
+                          customColors: CustomSliderColors(
+                            trackColor: Colors.grey,
+                            progressBarColor: Colors.black,
+                          ),
+                          size: 4.0,
+                          angleRange: 360,
+                          startAngle: -90.0,
+                        ),
+                        min: 0.0,
+                        max: 100.0,
+                        initialValue: _progressMiniPlayer,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 32.0,
+                      height: 32.0,
+                      child: Icon(_timer.isActive ? Icons.pause : Icons.play_arrow, size: 20),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
