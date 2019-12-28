1b61fd2687af3a1dca2211a6fd6930a1ed700c5a
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-12-28 12:19:29 +0700

    Created UI List Music Player

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,305 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyApp extends StatelessWidget {
+  final List<Music> listMusic = [
+    Music('State of Grace', 'Taylor Swift', 296),
+    Music('Red', 'Taylor Swift', 223),
+    Music('Treacherous', 'Taylor Swift', 243),
+    Music('I Knew You Were Trouble', 'Taylor Swift', 220),
+    Music('All Too Well', 'Taylor Swift', 329),
+    Music('22', 'Taylor Swift', 232),
+    Music('I Almost Do', 'Taylor Swift', 245),
+    Music('We Are Never Ever Getting Back Together', 'Taylor Swift', 193),
+    Music('Stay Stay Stay', 'Taylor Swift', 206),
+    Music('The Last Time - Gary Lightbody', 'Taylor Swift', 299),
+    Music('Holy Ground', 'Taylor Swift', 203),
+    Music('Sad Beautiful Tragic', 'Taylor Swift', 294),
+    Music('The Lucky One', 'Taylor Swift', 240),
+    Music('Everything Has Changed - Ed Sheeran', 'Taylor Swift', 245),
+    Music('Starlight', 'Taylor Swift', 221),
+    Music('Begin Again', 'Taylor Swift', 239),
+    Music('The Moment I Knew', 'Taylor Swift', 287),
+    Music('Come Back...Be Here', 'Taylor Swift', 224),
+    Music('Girl At Home', 'Taylor Swift', 221),
+    Music('Treacherous - Original Demo Recording', 'Taylor Swift', 240),
+    Music('Red - Original Demo Recording', 'Taylor Swift', 227),
+    Music('State Of Grace - Acoustic Version', 'Taylor Swift', 323),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double widthScreen = mediaQueryData.size.width;
+    double paddingBottom = mediaQueryData.padding.bottom;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        width: widthScreen,
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Column(
+              children: <Widget>[
+                _buildWidgetBackgroundCoverAlbum(widthScreen, context),
+                _buildWidgetListMusic(context, paddingBottom),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            _buildWidgetButtonPlayAll(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetListMusic(BuildContext context, double paddingBottom) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: paddingBottom > 0 ? paddingBottom : 16.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+            colors: [
+              Colors.blueGrey.withOpacity(0.5),
+              Colors.white.withOpacity(0.1),
+            ],
+            stops: [0.1, 0.9],
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 48.0),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    'Playlist',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                ),
+                WidgetShuffle(),
+                SizedBox(width: 24.0),
+                WidgetRepeat(),
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemBuilder: (context, index) {
+                  Music music = listMusic[index];
+                  int durationMinute = music.durationSecond >= 60 ? music.durationSecond ~/ 60 : 0;
+                  int durationSecond = music.durationSecond >= 60 ? music.durationSecond % 60 : music.durationSecond;
+                  String strDuration = "$durationMinute:$durationSecond";
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                music.title,
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              ),
+                              Text(
+                                '${music.artist} • $strDuration',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.more_vert),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: listMusic.length,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _buildWidgetButtonPlayAll() {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        width: 92,
+        decoration: BoxDecoration(
+          color: Color(0xFFAE1947),
+          borderRadius: BorderRadius.all(Radius.circular(48.0)),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10.0,
+              color: Color(0xFFAE1947),
+            ),
+          ],
+        ),
+        child: IconButton(
+          icon: Icon(Icons.play_arrow, color: Colors.white),
+          onPressed: () {},
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetBackgroundCoverAlbum(double widthScreen, BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/img_cover_album_red_taylor_swift.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.0),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black,
+                  Colors.black.withOpacity(0.1),
+                ],
+                stops: [
+                  0.0,
+                  0.7,
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: widthScreen / 2.5,
+              height: widthScreen / 2.5,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/img_cover_album_red_taylor_swift.jpeg'),
+                  fit: BoxFit.cover,
+                ),
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 15.0,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: 28.0,
+              height: 28.0,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(99)),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: widthScreen / 1.45),
+                SizedBox(height: 4.0),
+                Text(
+                  'Red (Deluxe Edition)',
+                  style: Theme.of(context).textTheme.title.merge(TextStyle(color: Colors.white)),
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  '22 Songs • 1 hr 30 min',
+                  style: Theme.of(context).textTheme.subtitle.merge(TextStyle(color: Colors.grey)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WidgetShuffle extends StatefulWidget {
+  @override
+  _WidgetShuffleState createState() => _WidgetShuffleState();
+}
+
+class _WidgetShuffleState extends State<WidgetShuffle> {
+  bool _isShuffle = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() => _isShuffle = !_isShuffle);
+      },
+      child: Icon(
+        Icons.shuffle,
+        color: Color(_isShuffle ? 0xFFAE1947 : 0xFF000000),
+      ),
+    );
+  }
+}
+
+class WidgetRepeat extends StatefulWidget {
+  @override
+  _WidgetRepeatState createState() => _WidgetRepeatState();
+}
+
+class _WidgetRepeatState extends State<WidgetRepeat> {
+  bool _isRepeat = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() => _isRepeat = !_isRepeat);
+      },
+      child: Icon(
+        Icons.repeat,
+        color: _isRepeat ? Color(0xFFAE1947) : Color(0xFF000000),
+      ),
+    );
+  }
+}
+
+class Music {
+  String title;
+  String artist;
+  int durationSecond;
+
+  Music(this.title, this.artist, this.durationSecond);
 }
